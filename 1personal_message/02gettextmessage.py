9ce562ed4de75250ba465fc24870632cd78fd6f3
nasir17git <nasir17.dev@gmail.com>
2022-04-20 15:21:15 +0900

    Feat: 1personal_message 기능 추가
    모든 키는 보안 이슈로 인해 일부를 삭제하여 작동하지 않습니다.
    01,02,03.py는 참고용 파일이며 04Integration.py에 상세주석이 첨부되어있습니다.
    
    파일설명
    ---------
    01mktoken - 액세스토큰 생성에 관한 코드
    02gettextmessage - 텍스트 메세지 형식으로 발신하는 코드
    03eatraw - 리스트 메세지 형식으로 발신하는코드
             - 전체기능구현안되도 이거만으로 완성된것처럼 보일수있어서 날로먹기입니다.
    04Integration - 토큰생성과 리스트메세지 출력을 하나의 파일로 포함했습니다.

---
+++ 1personal_message/02gettextmessage.py
@@ -0,0 +1,32 @@
+# 참고 블로그
+# https://novice-engineers.tistory.com/11?category=908185
+# https://ai-creator.tistory.com/23
+
+import requests
+import json
+
+#1.
+
+with open(r"D:\python_workspace\kakao\kakao_code.json","r") as fp:
+    tokens = json.load(fp)
+
+url="https://kapi.kakao.com/v2/api/talk/memo/default/send"
+
+# kapi.kakao.com/v2/api/talk/memo/default/send 
+
+headers={
+    "Authorization" : "Bearer " + tokens["access_token"]
+}
+
+data={
+    "template_object": json.dumps({
+        "object_type":"text",
+        "text":"절반은했다ㅏㅏㅏㅏㅏㅏㅏ",
+        "link":{
+            "web_url":"www.google.com"
+        }
+    })
+}
+
+response = requests.post(url, headers=headers, data=data)
+response.status_code
---
+++ 1personal_message/03eatraw.py
@@ -0,0 +1,60 @@
+# 참고 블로그
+# https://novice-engineers.tistory.com/11?category=908185
+# https://ai-creator.tistory.com/23
+
+import json
+import requests
+
+with open(r"D:\python_workspace\kakao\kakao_code.json","r") as fp:
+    tokens = json.load(fp)
+
+url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+
+# 사용자 토큰
+headers={
+    "Authorization" : "Bearer " + tokens["access_token"]
+}
+
+template = {
+    "object_type" : "list",
+    "header_title" : "오늘의 의상 추천",
+    "header_link" : {
+        "web_url" : "www.google.com",
+        "mobile_web_url" : "www.google.com"
+    },
+    "contents" : [
+        {
+            "title" : "오늘의 당신에겐 티셔츠가 어울려요!",
+            "description" : "상의 추천",
+            "image_url" : "https://www.picoop.co.kr/data/goods/17/2020/05/1243_tmp_9253441e841e04a1b9c63e489ae6e54b7526view.JPG",
+            "image_width" : 50, "image_height" : 50,
+            "link" : {
+                "web_url" : "www.google.com",
+                "mobile_web_url" : "www.google.com"
+            }
+        },
+        {
+            "title" : "오늘의 당신에겐 청바지가 어울려요!",
+            "description" : "하의 추천",
+            "image_url" : "https://image.musinsa.com/mfile_s01/2022/01/21/3310353cbe48b3d8c3dcebbc3738f58b125136.jpg",
+            "image_width" : 50, "image_height" : 50,
+            "link" : {
+                "web_url" : "www.google.com",
+                "mobile_web_url" : "www.google.com"
+            }
+        }
+        
+    ]
+    
+}
+
+data = {
+    "template_object" : json.dumps(template)
+}
+
+response = requests.post(url, data=data, headers=headers)
+print(response.status_code)
+if response.json().get('result_code') == 0:
+    print('메시지를 성공적으로 보냈습니다.')
+else:
+    print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
